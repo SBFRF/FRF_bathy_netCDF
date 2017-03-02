18eabacbd5d3e6f21c38f45ac391bb42c517bf6e
Spike@mundaka <spicer.bak@usace.army.mil>
2017-03-02 10:06:05 -0500

    updated for new sblib

--- NaturalNeighborRuntimeExample.py
+++ NaturalNeighborRuntimeExample.py
@@ -7,23 +7,23 @@ import netCDF4 as nc
 import sblib as sb
 
 
-globalYaml = '/home/spike/CMTB/yaml_files/Survey/Transect_Survey_LARC_Global.yml' # location of yaml files for transect data
-varYaml = 'yaml_files/Survey/Transect_Survey_variables.yml' # location of yaml files for transect data
+globalYaml = 'transect_Global.yml' # location of yaml files for transect data
+varYaml = 'transect_variables.yml' # location of yaml files for transect data
 
-grid_global = 'yaml_files/Survey/grid_Global.yml'  # location of yaml files for gridded data
-grid_var = 'yaml_files/Survey/grid_variables.yml'  # location of yaml files for gridded data
+grid_global = 'grid_Global.yml'  # location of yaml files for gridded data
+grid_var = 'grid_variables.yml'  # location of yaml files for gridded data
 
 
-filelist = glob.glob('/home/spike/repos/frf_bathy_interp/data')
+filelist = glob.glob(globPath)
 
 for fname in filelist:
     try:
-        os.remove(file[:-4] + '.nc')
-        os.remove(file[:-4] + '_grid.nc')
+        os.remove(fname[:-4] + '.nc')
+        os.remove(fname[:-4] + '_grid.nc')
     except OSError:
         continue
     # first make transect
-    TransectDict = sb.load_FRF_Transect(fname)
+    TransectDict = sb.import_FRF_Transect(fname)
     ofname = fname[:-3] + 'nc'
     TransectDict['time'] = nc.date2num(TransectDict['time'], 'seconds since 1970-01-01')
     makenc.makenc_FRFTransect(bathyDict=TransectDict, ofname=ofname, globalYaml=globalYaml, varYaml=varYaml)
@@ -43,4 +43,4 @@ if __name__ == "__main__":
     opts, args = getopt.getopt(sys.argv[1:], "h", ["help"])
     #.
     #
-    fname = args[0]
+    globPath = args[0]
